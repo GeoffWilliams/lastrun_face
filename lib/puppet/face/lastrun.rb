57de9370102e235d00c6085b8466197bddb94c38
Geoff Williams <geoff.williams@puppetlabs.com>
2016-04-03 19:58:01 +1000

    use puppet config print to get platform independent address

--- lib/puppet/face/lastrun.rb
+++ lib/puppet/face/lastrun.rb
@@ -1,21 +1,25 @@
 require 'puppet'
 require 'puppet/face'
 
-Puppet::Face.define(:lastrun, '0.1.0')
+Puppet::Face.define(:lastrun, '0.1.0') do
   summary "Info on your last puppet run the easy way"
   copyright "Geoff Williams", 2016
   license "Apache 2"
 
-  CACHE_DIR = "/opt/puppetlabs/puppet/cache/state/"
-
   action :classes do
     summary "Classes loaded during last puppet run"
-    puts File.read("#{CACHE_DIR}/classes.txt")
+
+    when_invoked do |options|
+      puts File.read(%x[puppet config print classfile].strip)
+    end
   end
 
-  action :resources
+  action :resources do
     summary "Resources loaded during last puppet run"
-    puts File.read("#{CACHE_DIR}/resources.txt")
+
+    when_invoked do |options|
+      puts File.read(%x[puppet config print resourcefile].strip)
+    end
   end
 end
   
